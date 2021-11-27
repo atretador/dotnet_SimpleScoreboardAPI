08103484e6bc251287811440e36b3ce83a1981f1
atretador <christian.atre@gmail.com>
2021-11-27 05:20:26 -0300

    clean up

--- Models/Score.cs
+++ Models/Score.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace scoreboard.Models
 {
